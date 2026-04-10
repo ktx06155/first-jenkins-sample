106862c470ce2331af3a65bb63ad7813f5cccc82
Ubuntu <ubuntu@ip-172-31-2-59.ap-northeast-2.compute.internal>
2026-04-10 05:10:31 +0000

    code change 3

--- src/main/java/com/example/first_jenkins_sample/HelloController.java
+++ src/main/java/com/example/first_jenkins_sample/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, Jenkins CI/CD! Finally's final";
+        return "Hello, Jenkins CI/CD! I'm DOBBY!";
     }
 }
